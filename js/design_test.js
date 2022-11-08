cda7fddb9aba1fcfe5f1905b32ba8c5223fc9631
omaoma-code <84127594+omaoma-code@users.noreply.github.com>
2022-11-08 23:59:48 +0900

    パソコンからの変更
    json_test.htmlを変更して、アコーディオンと表の増減を追加

--- js/design_test.js
+++ js/design_test.js
@@ -8,7 +8,7 @@ $(function () {
   //.accordion_oneの中の.accordion_headerがクリックされたら
   $('#bt').click(function () {
     //クリックされた.accordion_oneの中の.accordion_headerに隣接する.accordion_innerの横幅を開いたり閉じたりする。
-    $('.inner_IC').animate({ width: 'toggle',fontSize: 'toggle'});
+    $('.inner_IC').animate({ "width": 'toggle', fontSize: 'toggle' }, 250);
     $('.inner_IC').toggleClass("open");
   });
 });
@@ -16,30 +16,30 @@ $(function () {
 let table;
 let colums;
 
-function getData(){
-fetch("../sample_json/sample_result.json") // jsonファイルの場所
-  .then((response) => {
-    return response.json();
-  })
-  .then((members) => {
-    console.log(members);
-    table = $('table')[0];
-    colums = table.insertRow();
-    members.parts.forEach(element => {
-      th = document.createElement('th');
-      th.append(element);
-      colums.append(th);
-    }); //foreach
-    
-    let cols = 
-    [members.colum.IC.have_1,members.colum.IC.have_2,members.colum.IC.dir_1,members.colum.IC.dir_2];
-    addDetail(cols,"IC");
+function getData() {
+  fetch("../sample_json/sample_result.json") // jsonファイルの場所
+    .then((response) => {
+      return response.json();
+    })
+    .then((members) => {
+      console.log(members);
+      table = $('table')[0];
+      colums = table.insertRow();
+      members.parts.forEach(element => {
+        th = document.createElement('th');
+        th.append(element);
+        colums.append(th);
+      }); //foreach
 
-    cols = 
-    [members.colum.WORK.have_work,members.colum.WORK.dir_work];
-    addDetail(cols,"WORK");
+      let cols =
+        [members.colum.IC.have_1, members.colum.IC.have_2, members.colum.IC.dir_1, members.colum.IC.dir_2];
+      addDetail(cols, "IC");
 
-  })
+      cols =
+        [members.colum.WORK.have_work, members.colum.WORK.dir_work];
+      addDetail(cols, "WORK");
+
+    })
 }
 
 
@@ -48,10 +48,10 @@ fetch("../sample_json/sample_result.json") // jsonファイルの場所
  * @param {配列} members 
  * @param {*} name 
  */
-function addDetail(members,name){
+function addDetail(members, name) {
   let index = 0;
-  for(index = 0;index < colums.children.length;index++){
-    if(colums.children[index].textContent === name)
+  for (index = 0; index < colums.children.length; index++) {
+    if (colums.children[index].textContent === name)
       break;
   }
 
@@ -59,6 +59,6 @@ function addDetail(members,name){
     let th = colums.insertCell(++index);
     th.append(element);
     th.classList.add("inner_" + name);
-    th.outerHTML = th.outerHTML.replace('<td','<th');
+    th.outerHTML = th.outerHTML.replace('<td', '<th');
   });
 }
